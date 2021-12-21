3f963d825772a91299e3e300a33c0ef64ea9908e
Heather Bjoin <h.m.bjoin@gmail.com>
2021-12-21 08:38:37 -0600

    And so it begins. Adding folders, react, other libraries, starting header

---
+++ client/src/components/header/Header.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+import cairnStones from './images/cairnstonesbsb2.jpg'
+
+const Header = () => (
+    <div>
+        <p>Hello from Header</p>
+        <div className="image"></div>
+    </div>
+);
+
+export default Header;
