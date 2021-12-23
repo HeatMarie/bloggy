a1eff1a7ecc503903b5af4242ddeb0bbc3e18a82
Heather Bjoin <h.m.bjoin@gmail.com>
2021-12-22 21:05:44 -0600

    Navbar with no errors. YAY! Still needs styled

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/header/Header';
+import ResponsiveAppBar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ResponsiveAppBar />
       <p>Hello World</p>
 
     </div>
--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import './header.css'
 
 
-const Header = () => (
+const Header: React.FC = () => (
     <div>
         <div className="header"></div>
     </div>
